c5950240f0282c296df8215cd6743d7ed99117a2
0leum <ritam.d3v@gmail.com>
2025-02-25 00:10:15 +0530

    chulkani

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
-import LinkAccountButton from "@/components/link-account-button"
-// import {Button} from "@/components/ui/button"
+"use client";
+// import LinkAccountButton from "@/components/link-account-button"
+import {Button} from "@/components/ui/button"
+import { signIn } from "next-auth/react"
 
 
 export default async function Home() {
-  // return <h1 className="text-red-600">Hello World</h1>
-  return <LinkAccountButton/>
+  const GoogleLogin = ()=> signIn ('google', {callbackUrl:'http://localhost:3000/home-page'})
+
+  // return <LinkAccountButton/>
+  return (
+    <Button onClick={GoogleLogin}>Sign in with Google</Button>
+  )
 }
