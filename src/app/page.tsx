1a7a52b6da8c51f61f245e7e6599562c0c9a9315
0leum <ritam.d3v@gmail.com>
2025-02-24 13:32:46 +0530

    aurinko integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-import {Button} from "@/components/ui/button"
+import LinkAccountButton from "@/components/link-account-button"
+// import {Button} from "@/components/ui/button"
 
 
 export default async function Home() {
   // return <h1 className="text-red-600">Hello World</h1>
-  return <Button>Hello World</Button>
+  return <LinkAccountButton/>
 }
